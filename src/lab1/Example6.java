1de4ab043820c96680c4e8299cf78195edd6225d
garipov-ar <disprogar@gmail.com>
2022-11-25 18:51:45 +0500

    Lr1

---
+++ src/lab1/Example6.java
@@ -0,0 +1,21 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Example6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите Фамилию: ");
+        String sname = in.nextLine();
+
+        System.out.println("Введите Имя: ");
+        String name = in.nextLine();
+
+        System.out.println("Введите Отчество: ");
+        String otchestvo = in.nextLine();
+
+        System.out.print("Hello " + sname + " " + name + " " + otchestvo + "!");
+        in.close();
+    }
+}
---
+++ src/lab1/Example7.java
@@ -0,0 +1,17 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class Example7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите Имя: ");
+        String name = in.nextLine();
+
+        System.out.println("Введите возраст: ");
+        int age = in.nextInt();
+
+        System.out.println("Возраст пользователя " + name + " - " + age + " года");
+    }
+}
