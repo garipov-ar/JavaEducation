560e7f4357bb3f0fa0253a2edf0630b3ac1aef1a
garipov-ar <disprogar@gmail.com>
2022-11-28 18:08:04 +0500

    Lr1

--- src/lab1/Example15.java
+++ src/lab1/Example15.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Example15 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         System.out.println("Введите первое число ");
         int n1 = in.nextInt();
         System.out.println("Введите второе число ");
